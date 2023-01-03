7b6a8cc267705b59cee08028ae7f3177e2d3afe9
akshara2820 <akshara20@navgurukul.org>
2023-01-04 00:05:05 +0530

    first commit

---
+++ src/component/header.js
@@ -0,0 +1,18 @@
+function Header() {
+    return (
+        <>
+        <div className='header-section'>
+            <img src='../Assets/logo.png' alt='logo' />
+            <div className='nav-bar'>
+                <p>Home</p>
+                <p>Home</p>
+                <p>Home</p>
+                <p>Home</p>
+                <button className='button'>Start a Company</button>
+            </div>
+
+        </div>
+        </>
+    )
+}
+export default Header;
